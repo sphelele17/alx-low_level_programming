cb9e8b8dbdf9d4ebdb798aad1c0737a4888fb57e
sphelele17 <mbathaflorence380@gmail.com>
2023-09-12 10:28:14 -0700

    ASCII functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Print the alphabet in lowecase.
+ *
+ * Return: Always 0 (Success)
+ */
+vois print-alphabet(void)
+{
+	int i;
+
+	for ( i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
